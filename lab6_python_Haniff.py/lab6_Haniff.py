2c05f3c30122e3b0db667df13e2ea0cd73b578ad
HaniffAlisa <alisa.haniff95@student.qcc.cuny.edu>
2024-09-20 12:52:59 -0400

    updated example 1-Alisa

--- lab6_python_Haniff.py/lab6_Haniff.py
+++ lab6_python_Haniff.py/lab6_Haniff.py
@@ -5,17 +5,18 @@ Sept 20, 2024 Python Classes
 
 print("/n-------Example 1: exception handling-----")
 def hour_ratio():
-    hours = 24
-    h = int(input("Please enter a number to divide 24 hours: "))
+    try:
+        hours = 24
+        h = int(input("Please enter a number to divide 24 hours: "))
 
-    hours /=h #hours = hours/h
-    return hours
-except ZeroDivisionError:
-    print(f"The number {h} can't be divided by 24. Result is undefined")
-except ValueError
-    print(f"Input value was not a number.")
-except:
-    print(f"Program has an error.")
+        hours /=h #hours = hours/h
+        return hours
+    except ZeroDivisionError:
+        print(f"The number {h} can't be divided by 24. Result is undefined")
+    except ValueError:
+        print(f"Input value was not a number.")
+    except:
+        print(f"Program has an error.")
 
 
 print(hour_ratio())
