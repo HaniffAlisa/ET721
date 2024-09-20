fb02f437066d5943dd2656d655ed9e7ec8963d51
HaniffAlisa <alisa.haniff95@student.qcc.cuny.edu>
2024-09-20 13:49:42 -0400

    started excerise6_bank account-Alisa

---
+++ lab6_python_Haniff.py/exercise_lab6_Haniff.py
@@ -0,0 +1,10 @@
+"""Alisa Haniff
+Excerise 6 Lab 6
+python class"""
+
+class BankAccount:
+    def __init__(self, account_number, account_holder, balance):
+        self.account_number = account_number
+        self.account_holder = account_holder
+        self.balance = balance
+        
