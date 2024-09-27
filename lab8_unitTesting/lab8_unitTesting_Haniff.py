b7d9f0fae011b4d97954980029d196af0a694f5b
Alisa Haniff <alisa.haniff95@student.qcc.cuny.edu>
2024-09-27 13:20:05 -0400

    calculation files-alisa

--- lab8_unitTesting/lab8_unitTesting_Haniff.py
+++ lab8_unitTesting/lab8_unitTesting_Haniff.py
@@ -6,11 +6,15 @@ Unit Testing
 
 import unittest
 
-
+print(f"\n--------Example #1 simple function unit test 1 file---------\n")
 def addTwoNumbers(a,b):
     return a+b
 
 #create a unit tes to check if the function 'addTwoNumbers' is working properly
 class TestAddFunction(unittest.TestCase):
     def test_addTwoNumbers(self):
-        
+        self.assertEqual(addTwoNumbers(3,5),8)
+
+if __name__ == '__main__':
+    unittest.main()
+
