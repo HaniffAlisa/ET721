e2f275a0be4caefdb266f70bec156e6df97f6070
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-11-04 15:50:57 +0000

    django pyproject

--- lab11_django_dolist/dolist/models.py
+++ lab11_django_dolist/dolist/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
-# Create your models here.
+"""class Todolist(models.Model):
+    title = models.CharField(max_length=100, default="Untitled Task")  # Added default
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+"""
+
 class Todolist(models.Model):
-    text = models.CharField(max_length=45)
-    completed = models.BooleanField(default=False)
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    completed = models.BooleanField(default=False)  # Checkbox field for completion status
+
+    def __str__(self):
+        return self.title
--- lab11_django_dolist/dolist/urls.py
+++ lab11_django_dolist/dolist/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from . import views 
-
+from . import views # . means import from the same directory
 urlpatterns = [
-    #load page of the app will be sent to th 'index.html' file
-    path( "", views.index, name='index')
+#load page of the app will be sent to the 'index.html' file
+path('', views.index, name='index'),
 ]
