dcb250fc84c1fafc620aa862dedb6387b1fce5f6
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-12-09 23:00:58 +0000

    working. with the home page

--- lab10_completed_Django/dolist/test_views.py
+++ lab10_completed_Django/dolist/test_views.py
@@ -2,19 +2,19 @@ from django.test import TestCase
 from django.urls import reverse
 from .models import Todolist
 from .forms import TodoListForm
-from views import addTodoItem, completedTodo
+from dolist.views import addTodoItem, completedTodo
 
 # Create your tests here.
 class TodoViewsTestCase(TestCase):
     def setUp(self):
-        self.task1= Todolist.objects.create(text="Task 1", completed = False)
-        self.task2= Todolist.objects.create(text="Task 1", completed = False)
-        self.task3= Todolist.objects.create(text="Task 1", completed = True)
+        self.task1= Todolist.objects.create(title="Task 1", completed = False)
+        self.task2= Todolist.objects.create(title="Task 2", completed = False)
+        self.task3= Todolist.objects.create(title="Task 3", completed = True)
 
     #test index view if it renders the correct context and template
     def test_index_views(self):
         response = self.client.get(reverse('index'))
-        self.assertEqual(response.sttus_cod, 200)
+        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
 
         self.assertQuerySetEqual(
@@ -22,20 +22,20 @@ class TodoViewsTestCase(TestCase):
             Todolist.objects.order_by('id'),
             transform=lambda x:x,
         )
-        self.assetIsInstance(response.context['form'], TodoListForm)
+        self.assertIsInstance(response.context['form'], TodoListForm)
 
         #test adding a valid todo item
-        def test_add_todo_item_view_valid_data(self):
-            response = self.client.post(reverse(addTodoItem), {'text': 'Task 3'})
+    def test_add_todo_item_view_valid_data(self):
+            response = self.client.post(reverse(addTodoItem), {'title': 'Task 3'})
             self.assertEqual(response.status_code, 302)
 
             #verify the new task was added
-            self.assertEqual(Todolist.obect.count(), 3)
-            self.assertTrue(Todolist.objects.filter(text="Task 3"). exists())
+            self.assertEqual(Todolist.objects.count(), 3)
+            self.assertTrue(Todolist.objects.filter(title="Task 3"). exists())
 
         #test marking a valid to do complete
-        def test_completed_todo_valid(self):
+    def test_completed_todo_valid(self):
             response = self.client.post(reverse(completedTodo, args =[self.task1.id]))
             self.assertEqual(response.status_code, 302)
-            self.task1.refresh_form_db()
+            self.task1.refresh_from_db()
             self.assertTrue(self.task1.completed)    
--- lab10_completed_Django/dolist/urls.py
+++ lab10_completed_Django/dolist/urls.py
@@ -4,4 +4,17 @@ from . import views             # . means import from the same directory
 urlpatterns = [
     #load page of the app will be sent to the 'index.html' file
     path('', views.index, name='index'),
+
+    #add new task into list
+    path('add', views.addTodoItem, name='add'),
+
+    #mark task as completed
+    path('completed/<int:todo_id>/', views.completedTodo, name='completed'),
+    
+    #delete task that are marked as completed
+    path('deletecompleted', views.deletecompleted, name = 'deletecompleted'),
+
+    #delete all tasks
+    path('deleteall', views.deleteAll, name='deleteall'),
+    
 ]
