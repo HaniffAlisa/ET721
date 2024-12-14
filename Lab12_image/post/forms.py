1b6ab651bd26bb34d6ba265610bd5ec60cd3362e
HaniffAlisa <154397440+HaniffAlisa@users.noreply.github.com>
2024-12-14 00:56:19 +0000

    completed Upload Images

--- Lab12_image/post/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from . models import Post
-
-class PostForm(forms.ModeForm):
-    class Meta:
-        model =Postfields =['title', 'image']
--- Lab12_image/post/urls.py
+++
@@ -1,7 +0,0 @@
-from . views import HomePageView, CreatePostView
-from django.urls import path
-
-urlpatterns = [
-    path('', HomePageView.as_view(), name = 'home' ),
-    path('post', CreatePostView.as_view(), name = 'add_post'),
-]
